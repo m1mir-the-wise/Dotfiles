996faa68eeb13551c163c0c0d60effebe8342208
m1mir-the-wise <gu-germiniani@hotmail.com>
2025-07-07 13:16:18 -0300

    Ajustes do fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,22 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -g theme_display_user yes
-set -g theme_display_hostname yes
 set -g theme_nerd_fonts yes
 set -g theme_color_scheme dracula
+
+set -g theme_display_user yes
+set -g theme_display_hostname yes
+
 set -g theme_display_git yes
+set -g theme_git_worktree_support yes
+
+set -g theme_display_time yes
+set -g theme_display_time_format "%H:%M"  
+
+set -g theme_display_jobs yes
 set -g theme_show_exit_status yes
-set -g theme_newline_cursor underscore
+set -g theme_newline_cursor block
+
 alias ls='exa --icons'
 alias update='sudo pacman -Syu'
 alias pipes='pipes.sh -c 2'
