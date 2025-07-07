31ca8a2b342cf611c1fe5f6569de388c5fa82f27
m1mir-the-wise <gu-germiniani@hotmail.com>
2025-07-07 13:37:18 -0300

    Ajustes do fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,22 +2,31 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Nerd Font
 set -g theme_nerd_fonts yes
-set -g theme_color_scheme dracula
 
+# Estilo pastel bonito
+set -g theme_color_scheme zenburn  # ou dracula, ou solarized-dark
+
+# Mostrar usuário + host
 set -g theme_display_user yes
 set -g theme_display_hostname yes
 
+# Mostrar diretório encurtado
+set -g theme_display_path yes
+set -g theme_display_path_short yes
+
+# Mostrar Git
 set -g theme_display_git yes
 set -g theme_git_worktree_support yes
 
+# Relógio com ícone nerd font
 set -g theme_display_time yes
-set -g theme_display_time_format "%H:%M"  
+set -g theme_display_time_format "%H:%M"
 
+# Visual extra
 set -g theme_display_jobs yes
 set -g theme_show_exit_status yes
-set -g theme_newline_cursor block
-
-alias ls='exa --icons'
+set -g theme_newline_cursor blockalias ls='exa --icons'
 alias update='sudo pacman -Syu'
 alias pipes='pipes.sh -c 2'
