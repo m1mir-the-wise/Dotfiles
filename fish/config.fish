ea40f5911f69e89c6c801a3b787a063c704b8f39
m1mir-the-wise <gu-germiniani@hotmail.com>
2025-07-09 02:59:43 -0300

    Atualização Automátizada

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,21 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# function full_path
+#     set cwd (pwd)
+#     set home (echo $HOME)
+#     if test (string match -r "^$home" $cwd)
+#         set cwd " ~"(string sub -s (math (string length $home) + 1) -- $cwd)
+#     end
+#     echo $cwd
+# end
+#
+# function fish_prompt
+#     set_color green
+#     echo -n (full_path) '> '
+#     set_color normal
+# end
+
 set -g fish_greeting
 
 alias gc="git add . && git commit -m 'Atualização Automátizada' && git push"
