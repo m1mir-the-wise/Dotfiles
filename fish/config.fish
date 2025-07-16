4c519755fe8a3b00075cc5d122a59b61bd6720ff
m1mir-the-wise <gu-germiniani@hotmail.com>
2025-07-16 19:18:29 -0300

    Adição de comentário explicativo do alias tmuxc no config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -25,6 +25,6 @@ alias gc="git add . && git commit && git push"
 alias fetch="bash ~/dotfiles/fetch/fetch.sh"
 alias pipes="pipes.sh -c 2"
 alias sync_simplicity="sudo rm -rf /usr/share/sddm/themes/simplicity && sudo cp -r ~/dotfiles/simplicity /usr/share/sddm/themes/"
-alias tmuxc="cat commands_tmux.txt"
+alias tmuxc="cat commands_tmux.txt" # create a file with this name with the tmux commands on the ~ directory, or just comment this line
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
